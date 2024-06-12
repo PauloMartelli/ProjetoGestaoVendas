9dedad6194b01ed1a98238888d931b0fa8b562a9
PauloI360 <paulo.martelli.itera360@hotmail.com>
2024-06-11 21:31:42 -0300

    Adicionando Entidade Venda e TipoPagamento

---
+++ ProjetoGestaoVendas.Dominio/Entidades/Venda.cs
@@ -0,0 +1,12 @@
+using ProjetoGestaoVendas.Dominio.Enumeradores;
+
+namespace ProjetoGestaoVendas.Dominio.Entidades;
+
+public class Venda
+{
+    public int ID { get; set; }
+    public decimal Valor { get; set; }
+    public TipoPagamento TipoPagamento { get; set; }
+    public DateTime DataEHora { get; set; }
+    public bool Ativo { get; set; }
+}
